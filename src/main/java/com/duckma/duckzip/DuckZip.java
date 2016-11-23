d8b2199e2c254c20d7b2619a86506aba71b69ffb
Federico Matera <federico@duckma.com>
2016-11-23 16:39:00 +0100

    zip method added

--- src/main/java/com/duckma/duckzip/DuckZip.java
+++ src/main/java/com/duckma/duckzip/DuckZip.java
@@ -1,14 +1,22 @@
 package com.duckma.duckzip;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
 import io.reactivex.FlowableOnSubscribe;
-
-import java.io.*;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
 
 public class DuckZip {
 
@@ -21,16 +29,16 @@ public class DuckZip {
 
         private int millisInterval;
 
-        UpdateInterval(int millisInterval){
+        UpdateInterval(int millisInterval) {
             this.millisInterval = millisInterval;
         }
 
-        int getMillisInterval(){
+        int getMillisInterval() {
             return millisInterval;
         }
     }
 
-    public DuckZip(){
+    public DuckZip() {
         this.currentMillisCheck = new CurrentMillisCheck() {
             @Override
             public long currentTimeMillis() {
@@ -39,18 +47,19 @@ public class DuckZip {
         };
     }
 
-    DuckZip(CurrentMillisCheck currentMillisCheck){
+    DuckZip(CurrentMillisCheck currentMillisCheck) {
         this.currentMillisCheck = currentMillisCheck;
     }
 
     /**
      * Extracts the files in the archive at sourceFilePath to the destDirectoryPath.
      * It emits updates with the percentage of file extracted with a given interval.
-     * @param sourceFilePath The path of the source archive file
+     *
+     * @param sourceFilePath        The path of the source archive file
      * @param updatesIntervalMillis Number of milliseconds to wait between consecutive
      *                              progress updates. Default 300ms, minimum 100ms.
      *                              Notice the lower the interval, the slower the unzipping process.
-     * @param destDirectoryPath The path for the destination directory
+     * @param destDirectoryPath     The path for the destination directory
      * @return An {@link Flowable} emitting updates indicating the percentage of files extracted.
      */
     public Flowable<Float> unzip(final String sourceFilePath, final String destDirectoryPath,
@@ -60,18 +69,18 @@ public class DuckZip {
             public void subscribe(final FlowableEmitter<Float> emitter) {
                 try {
                     unzip(
-                            sourceFilePath,
-                            destDirectoryPath,
-                            new UnzipProgressUpdateCallback() {
-                                @Override
-                                public void onUnzipProgressUpdate(Float progressUpdate) {
-                                    emitter.onNext(progressUpdate);
-                                }
-                            },
-                            updatesIntervalMillis.getMillisInterval() >
-                                    UpdateInterval.UPDATE_INTERVAL_FAST.getMillisInterval() ?
-                                    updatesIntervalMillis :
-                                    UpdateInterval.UPDATE_INTERVAL_FAST);
+                        sourceFilePath,
+                        destDirectoryPath,
+                        new UnzipProgressUpdateCallback() {
+                            @Override
+                            public void onUnzipProgressUpdate(Float progressUpdate) {
+                                emitter.onNext(progressUpdate);
+                            }
+                        },
+                        updatesIntervalMillis.getMillisInterval() >
+                            UpdateInterval.UPDATE_INTERVAL_FAST.getMillisInterval() ?
+                            updatesIntervalMillis :
+                            UpdateInterval.UPDATE_INTERVAL_FAST);
                     emitter.onComplete();
                 } catch (IOException ex) {
                     emitter.onError(ex);
@@ -88,21 +97,22 @@ public class DuckZip {
      * Unzips the archive from the sourceFilePath into the destDirectoryPath.
      * It emits progress updates through the {@link UnzipProgressUpdateCallback} every
      * updateIntervalMillis.
-     * @param sourceFilePath The path of the archive to unzip.
-     * @param destDirectoryPath The destination path for the unzip process.
+     *
+     * @param sourceFilePath              The path of the archive to unzip.
+     * @param destDirectoryPath           The destination path for the unzip process.
      * @param unzipProgressUpdateCallback A callback to receive progress updates.
-     * @param updateIntervalMillis The interval at which the updates will be fired.
+     * @param updateIntervalMillis        The interval at which the updates will be fired.
      * @throws IOException If source or destination paths can't be accessed.
      */
     public void unzip(String sourceFilePath, String destDirectoryPath,
                       UnzipProgressUpdateCallback unzipProgressUpdateCallback,
-                      UpdateInterval updateIntervalMillis) throws IOException{
+                      UpdateInterval updateIntervalMillis) throws IOException {
 
         //Input sanitizing
-        if(sourceFilePath == null){
+        if (sourceFilePath == null) {
             throw new IllegalArgumentException("Please provide a sourceFilePath");
         }
-        if(destDirectoryPath == null){
+        if (destDirectoryPath == null) {
             throw new IllegalArgumentException("Please provide a destinationFilePath");
         }
 
@@ -110,7 +120,7 @@ public class DuckZip {
         File destinationDirectory = new File(destDirectoryPath);
 
         ZipInputStream zis = new ZipInputStream(
-                new BufferedInputStream(new FileInputStream(sourceFile)));
+            new BufferedInputStream(new FileInputStream(sourceFile)));
 
         int fileCount = new ZipFile(sourceFile).size();
 
@@ -125,8 +135,8 @@ public class DuckZip {
                 currentFileIndex++;
                 // Only send progress updates if the given updateIntervalMillis has elapsed.
                 long currentTimeMillis = currentMillisCheck.currentTimeMillis();
-                if(currentTimeMillis - lastUpdateTime > updateIntervalMillis.getMillisInterval()) {
-                    unzipProgressUpdateCallback.onUnzipProgressUpdate((float)currentFileIndex/fileCount);
+                if (currentTimeMillis - lastUpdateTime > updateIntervalMillis.getMillisInterval()) {
+                    unzipProgressUpdateCallback.onUnzipProgressUpdate((float) currentFileIndex / fileCount);
                     lastUpdateTime = currentTimeMillis;
                 }
 
@@ -134,7 +144,7 @@ public class DuckZip {
                 File dir = ze.isDirectory() ? outputFile : outputFile.getParentFile();
                 if (!dir.isDirectory() && !dir.mkdirs()) {
                     throw new FileNotFoundException("Failed to ensure directory: " +
-                            dir.getAbsolutePath());
+                        dir.getAbsolutePath());
                 }
                 if (ze.isDirectory()) {
                     continue;
@@ -160,8 +170,111 @@ public class DuckZip {
         unzip(sourceFilePath, destDirectoryPath, unzipProgressUpdateCallback, UpdateInterval.UPDATE_INTERVAL_DEFAULT);
     }
 
+    /**
+     * Compress the file(s) at sourcePath to the destFilePath.
+     * It emits updates with the percentage of file compressed with a given interval.
+     *
+     * @param sourcePath The path of the source. It can be either a file or a directory
+     * @param destFilePath The path of the destination zip file
+     * @param updateInterval Number of milliseconds to wait between consecutive
+     *                       progress updates. Default 300ms, minimum 100ms.
+     *                       Notice the lower the interval, the slower the zipping process.
+     * @return An {@link Flowable} emitting updates indicating the percentage of files compressed.
+     */
+    public Flowable<Float> zip(final String sourcePath, final String destFilePath,
+                               UpdateInterval updateInterval) {
+        return Flowable.create(new FlowableOnSubscribe<Float>() {
+            @Override
+            public void subscribe(FlowableEmitter<Float> emitter) throws Exception {
+                zip(sourcePath, destFilePath, new ZipProgressUpdateCallback() {
+                    @Override
+                    public void onZipProgressUpdate(Float progressUpdate) {
+                        emitter.onNext(progressUpdate);
+                    }
+                }, updateInterval);
+                emitter.onComplete();
+            }
+        }, BackpressureStrategy.DROP);
+    }
+
+    /**
+     * Zips the file/directory from the sourcePath into the destFilePath.
+     * It emits progress updates through the {@link ZipProgressUpdateCallback} every
+     * updateIntervalMillis.
+     *
+     * @param sourcePath                The path of the archive to unzip.
+     * @param destFilePath              The destination path for the unzip process.
+     * @param zipProgressUpdateCallback The interval at which the updates will be fired.
+     * @param updateInterval            A callback to receive progress updates.
+     * @throws IOException
+     */
+    public void zip(String sourcePath, String destFilePath,
+                    ZipProgressUpdateCallback zipProgressUpdateCallback,
+                    UpdateInterval updateInterval) throws IOException {
+        File zipFile = new File(destFilePath);
+        if (zipFile.exists()) {
+            zipFile.delete();
+        }
+
+        FileOutputStream zipFos = new FileOutputStream(zipFile);
+        ZipOutputStream zos = new ZipOutputStream(zipFos);
+        File root = new File(sourcePath);
+        List<File> files = getFiles(root);
+        long lastUpdateTime = System.currentTimeMillis();
+
+        for (int i = 0; i < files.size(); i++) {
+            long currentTimeMillis = currentMillisCheck.currentTimeMillis();
+            if (currentTimeMillis - lastUpdateTime > updateInterval.getMillisInterval()) {
+                zipProgressUpdateCallback.onZipProgressUpdate((float) i / files.size());
+                lastUpdateTime = currentTimeMillis;
+            }
+
+            File file = files.get(i);
+            String path = file.getPath()
+                .substring(root.isDirectory() ? root.getPath().length() : root.getParent().length());
+
+            ZipEntry zipEntry = new ZipEntry(path);
+            zos.putNextEntry(zipEntry);
+
+            FileInputStream fis = new FileInputStream(new File(file.getAbsolutePath()));
+            byte[] bytes = new byte[1024];
+            int length;
+            while ((length = fis.read(bytes)) >= 0) {
+                zos.write(bytes, 0, length);
+            }
+            fis.close();
+            zos.closeEntry();
+        }
+
+        zos.finish();
+        zos.close();
+    }
+
+    private List<File> getFiles(File file) {
+        List<File> result = new ArrayList<>();
+        if (!file.isDirectory()) {
+            result.add(file);
+        } else {
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File child : files) {
+                    result.addAll(getFiles(child));
+                }
+            }
+        }
+        return result;
+    }
+
+    public Flowable<Float> zip(String sourcePath, final String destFilePath) {
+        return zip(sourcePath, destFilePath, UpdateInterval.UPDATE_INTERVAL_DEFAULT);
+    }
+
     public interface UnzipProgressUpdateCallback {
         void onUnzipProgressUpdate(Float progressUpdate);
+    }
+
+    public interface ZipProgressUpdateCallback {
+        void onZipProgressUpdate(Float progressUpdate);
     }
 
     interface CurrentMillisCheck {
